70c90e24e8b7e9a557335e33518833b998632474
Dev_Alexander <alexander.cardona@quind.io>
2023-04-26 15:33:08 -0500

    Se crea Proyecto Tutorial ASP.NET Web Forms

---
+++ wingtipToys/wingtipToys/Site.Master.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Web.UI;
+using wingtipToys.Models;
+
+namespace wingtipToys
+{
+	public partial class SiteMaster : MasterPage
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}	
+
+        public IQueryable<Category> GetCategories()
+        {
+			var _db = new WingtipToys.Models.ProductContext();
+			IQueryable<Category> query = _db.Categories;
+			return query;
+        }
+
+    }
+}
