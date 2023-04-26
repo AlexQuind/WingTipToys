05aa2abd6412a8b34e44c60e680d8afcdc6d5115
Dev_Alexander <alexander.cardona@quind.io>
2023-04-26 17:03:50 -0500

    Se un listado de productos y control de datos para navegación.

--- wingtipToys/wingtipToys/ProductList.aspx.cs
+++ wingtipToys/wingtipToys/ProductList.aspx.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.ModelBinding;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using wingtipToys.Models;
 
 namespace wingtipToys
 {
@@ -12,6 +14,16 @@ namespace wingtipToys
 		protected void Page_Load(object sender, EventArgs e)
 		{
 
+		}
+		public IQueryable<Product> GetProducts([QueryString("id")] int? categoryId)
+		{
+			var _db = new WingtipToys.Models.ProductContext();
+			IQueryable<Product> query = _db.Products;
+			if (categoryId.HasValue && categoryId > 0)
+			{
+				query = query.Where(p => p.CategoryID == categoryId);
+			}
+			return query;
 		}
 	}
 }
